2f59b7b9878620b5bada347a2bdbf242f85903db
kiolaksono <kiolaksono@gmail.com>
2024-10-01 17:59:04 +0700

    D1C5 - server-side - create postLogin

--- server-side/controllers/customerController.js
+++ server-side/controllers/customerController.js
@@ -1,4 +1,6 @@
 const {Customer} = require('../models')
+const {comparePass} = require('../helpers/hash')
+const { signToken } = require('../helpers/jwt')
 
 class CustomerController {
     static async getAllCustomer(req,res,next){
@@ -11,6 +13,33 @@ class CustomerController {
             res.status(200).json(data)
         } catch (error) {
             next(error)
+        }
+    }
+
+    static async postLogin(req,res,next){
+        try {
+            const {email, password} = req.body
+
+            if (!email) throw ({ name: "BadRequest", message: "Email is required" })
+            if (!password) throw ({ name: "BadRequest", message: "Password is required" })    
+
+            const find = await Customer.findOne({where:{email}})
+            if(!find) throw ({name:"Bad Request", message:"Invalid username/password"})
+            
+            // console.log(find)
+
+            const isValidPassword = comparePass(password, find.password)
+            
+            if(!isValidPassword) throw ({name:"Bad Request", message:"Invalid username/password"})
+                
+            const accessToken = signToken({id:find.id})
+            console.log("SDSD")
+
+            res.status(200).json({accessToken})
+
+        } catch (error) {
+            res.json(error)
+            // next(error)
         }
     }
 }
--- server-side/routers/customer.js
+++ server-side/routers/customer.js
@@ -5,5 +5,7 @@ const CustomerController = require('../controllers/customerController')
 const customers = express()
 
 customers.get("/", CustomerController.getAllCustomer)
+customers.post("/login", CustomerController.postLogin)
+
 
 module.exports = customers

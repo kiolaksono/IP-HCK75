cd3e475fc6e9935a403b87b8d517f9c0358fcc55
kiolaksono <kiolaksono@gmail.com>
2024-10-04 11:27:22 +0700

    D5C4 - client-side - Final Commit

--- client-side/src/components/Button.jsx
+++ client-side/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({name, onClick}){
 
     return(
-        <button onClick={onClick} className={name==="logout" ? "btn btn-ghost text-white" : "btn btn-primary text-white"} >{name}</button>
+        <button onClick={onClick} className={name==="logout" ? "btn btn-ghost w-16 text-white " : "btn btn-primary w-16 text-white"} >{name}</button>
     )
 }
